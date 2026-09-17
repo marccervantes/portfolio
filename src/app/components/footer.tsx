525b7c049d5105eda8d0c2349f90a30c4c0ffefc
marccervantes <marccg2000@hotmail.com>
2026-09-17 19:14:36 +0100

    Add games link in footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,6 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-100 mt-12 py-6 text-center text-sm text-gray-600">
       © {new Date().getFullYear()} - Designed <span className='font-sans'>&</span> coded by <span><a href='https://www.linkedin.com/in/marc-cervantes-garcia/'>Marc Cervantes Garcia</a></span>
+      <br />Play my IA games <a href='https://games.marccervantes.com' rel='noopener noreferrer' className='text-blue-500 sunderline'>here</a>
     </footer>
   );
 };
